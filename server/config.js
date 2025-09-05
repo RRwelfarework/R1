0101ea3910468cec9d30d5a4402720ab1ad5f132
RISHABH RATHORE <163635165+RRwelfarework@users.noreply.github.com>
2025-09-05 11:45:53 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://arpit:8009138392@cluster0.y0nburv.mongodb.net/',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://rishabhrathorebtech1999_db_user:7880407722@cluster0.asx5jry.mongodb.net/',
   JWT_SECRET: process.env.JWT_SECRET || 'r59h3B2!jA0sK1qZ8oTzP0xL3eT9uD5vW9lI7jBzX6qY8oV',
   ADMIN_EMAIL: process.env.ADMIN_EMAIL || 'rishabh_2312res940@iitp.ac.in',
   ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || 'RishabhRathore940@123',
